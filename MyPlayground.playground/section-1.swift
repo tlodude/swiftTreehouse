32dd7e8822f559b60d67a4036e731319ae4fa3ce
Tom Long <tllong50@gmail.com>
2015-02-01 13:12:25 -0800

    first commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str : String = "Hello "
+
+let modernProgrammingLanguage: String = "Swift"
+
+var geeeting = "\(str) \(modernProgrammingLanguage)"
+
+println("To build a modern iPhone app, we need to learn \(modernProgrammingLanguage)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
