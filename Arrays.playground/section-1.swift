22821ff8e7cfa082669c7b4a69252a213c6a81e7
Tom Long <tllong50@gmail.com>
2015-02-01 18:46:23 -0800

    added Unary, Arrays, and Dictionary

---
+++ Arrays.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var todo: [String] = ["Return Calls","Write Blogpost", "Cook Dinner"]
+
+todo += ["Pickup Laundry", "Buy Bulbs"]
+
+todo[0]
+
+todo.count
+
+todo.append("Edit Blog Post")
+todo
+
+
+todo[2] = "Clean Dishes"
+todo
+
+
+let item = todo.removeLast()
+item
+
+let item2 = todo.removeAtIndex(1)
+item2
+
+todo.insert("Call Mom", atIndex: 1)
+todo
+
+
+
--- Unary.playground/section-1.swift
+++ Unary.playground/section-1.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var levelScore = 0
+
+levelScore--
+levelScore
+
+var totalScore = levelScore--
+
+totalScore
+levelScore
+
+// == increment unary operator
+// -- decrement unary operator
+// - negative operator
+
+levelScore = -levelScore
+
+// ! NOT Operator
+
+let on = false
+let off = !on
