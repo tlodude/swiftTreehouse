6ab5234dc00493ff69bacc51a0cf6cdf076a479a
Tom Long <tllong50@gmail.com>
2015-02-01 17:42:59 -0800

    added Numbers, Operator, and OperatorPrecedence playgrounds

---
+++ Numbers.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let language: String = "Swift"
+
+let year: Int = 2014 // Int or Integer
+
+var version: Float = 1.0 // Double = 15 decimal and Float = 7 decimal
+
+var isAwesome: Bool = true // Boolean or Bool
+
+
---
+++ OperatorPrecedence.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var x = (100 + 100 - 5) * 2 / 3 % 7
+
+100 + 100
+200 - 5
+195 * 2
+390 / 3
+130 % 7
+
